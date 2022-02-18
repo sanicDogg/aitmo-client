2888ee2ea1326456076cff536bd19530054f57df
sanicDogg <>
2022-02-18 06:08:13 +0300

    Commit #15 Eslint temporary removed

--- src/components/ChannelsContext.js
+++ src/components/ChannelsContext.js
@@ -1,4 +1,3 @@
 import React from 'react';
 
-// eslint-disable-next-line import/prefer-default-export
 export const ChannelsContext = React.createContext();
--- src/components/Stream/StreamContext.js
+++ src/components/Stream/StreamContext.js
@@ -1,4 +1,3 @@
 import React from 'react';
 
-// eslint-disable-next-line import/prefer-default-export
 export const StreamContext = React.createContext();
